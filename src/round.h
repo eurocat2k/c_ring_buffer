fd8ae248960bc059cda3e6b3845a44a5dde228e2
eurocat2k <gabor.zelenak@me.com>
2024-07-16 11:53:48 +0000

    Create round.h

---
+++ src/round.h
@@ -0,0 +1,16 @@
+#ifndef __ROUND_H__
+#define __ROUND_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+
+int RoundToMSB(int);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __ROUND_H__
