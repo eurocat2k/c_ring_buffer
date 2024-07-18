ee48ced491b2c764f2812e393f5b4b3b61237edf
eurocat2k <gabor.zelenak@me.com>
2024-07-18 07:39:56 +0000

    Update hexdump.c

--- src/hexdump.c
+++ src/hexdump.c
@@ -1,97 +1,64 @@
-#include <stdio.h>
+#include <ctype.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/endian.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
+#include <unistd.h>
 
-#ifndef ASCII_BUF_SIZE
-#define ASCII_BUF_SIZE  16
-#endif // ASCII_BUF_SIZE
+#ifndef MIN_ASCII_CHAR
+#define MIN_ASCII_CHAR 0x21
+#endif
 
-void HexDump(const void *src, size_t len) {
-    bool odd = true;
-    char ch = '\0';
-    int wch = 0;
-    size_t offset = 0, size = len, left = size;
+#ifndef MAX_ASCII_CHAR
+#define MAX_ASCII_CHAR 0x7e
+#endif
+
+void HexDump(const void *src, size_t nbytes) {
     uint8_t *data = NULL;
     data = (uint8_t *)src;
-    uint8_t asciibuf[ASCII_BUF_SIZE + 1] = {0};
-    int lencnt = 0;
-    // , index = 0;
+    size_t i, j, k;
+    uint8_t ch = '.', as = '.';
+    printf("%-8s  00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  %-16s\n", "Address", "Ascii");
+    printf("%8s  =======================  =======================  %16s\n",
+           "========", "================");
+    for (i = 0; i < nbytes; i++) {
+        // ch = data[i];
+        if ((i % 16) == 0) {
+            printf("%08lx", i);
+        }
+        if ((i % 8) == 0) {
+            printf(" ");
+        }
+        printf("%3.02x", data[i]);
 
-    // header
-    //               00000 4C 6F 72 65 6D 20 49 70   73 75 6D 20 69 73 20 73  Lorem Ipsum is s
-    fprintf(stdout, "====== =======================   =======================  ================\n");
-    fprintf(stdout, "OFFSET 00 01 02 03 04 05 06 07   08 09 0A 0B 0C 0D 0E 0F  ASCII...........\n");
-    fprintf(stdout, "====== =======================   =======================  ================\n");
-    // fprintf(stdout, "[%zu] %06X", left, (unsigned)offset);
-    fprintf(stdout, "%06X", (unsigned)offset);
-    //
+        if (((i % 16) == 15) || (i == nbytes - 1)) {
 
-    // outer loop
-    for (size_t i = 0, index = 0; i < size; i += 1, left -= 1) {
-        ch = *(uint8_t*)(src + i);
-        asciibuf[index++] = ch;
-        // fprintf(stdout, "%3.02X", ch);
-        fprintf(stdout, "%3.02X", ch);
-        lencnt += 1;
-        if (((i + 1) % 8) == 0) {
-            if (odd) {
-                fprintf(stdout, "  ");
-                odd = false;
-            } else {
-                index = 0;
-                offset += 16;
-                fprintf(stdout, "  ");
-                // memset(asciibuf, 0, ASCII_BUF_SIZE);
-                for (size_t k = 0; k < 16; k += 1) {
-                    int _ch = asciibuf[k];
-                    if (isalnum(_ch) ||
-                        ispunct(_ch) ||
-                        isspace(_ch)) {
-                        fprintf(stdout, "%c", asciibuf[k]);
-                    } else {
-                        fprintf(stdout, ".");
-                    }
-                }
-                if (left > 1) {
-                    // fprintf(stdout, "\n[%zu] %06X", left, (unsigned)offset);
-                    fprintf(stdout, "\n%06X", (unsigned)offset);
-                }
-                odd = true;
-                lencnt = 0;
+            for (j = 0; j < 15 - (i % 16); j++) {
+                printf("   ");
             }
-        }
-        //
-    }
 
-    // PRINT OUT REMAINIG CHARACTERS FROM BUFFER AND FILL UP THE ASCII TAB
-    int rem = ASCII_BUF_SIZE - lencnt;
-
-    for (int i = rem; i > 0; i -= 1) {
-        if (rem == 8) {
-            fprintf(stdout, "   ");
-        } else {
-            if (i % 8 == 0) {
-                fprintf(stdout, "     ");
-            } else {
-                fprintf(stdout, "   ");
+            printf(" |");
+            k = 15;
+            for (j = (i - (i % 16)); j <= i; j++, k--) {
+                // as = data[j];
+                if ((data[j] >= MIN_ASCII_CHAR) && (data[j] <= MAX_ASCII_CHAR)) {
+                    printf("%c", data[j]);
+                } else {
+                    printf("%c", '.');
+                }
             }
+
+            if (k > 0 && k < 15) {
+                for (k += 1; k > 0; k--) {
+                    printf(" ");
+                }
+            }
+
+            printf("|\n");
         }
     }
-
-    fprintf(stdout, "  ");
-
-    for (int k = 0; k < lencnt; k += 1) {
-        if (isalnum(asciibuf[k]) || ispunct(asciibuf[k]) ||
-            isspace(asciibuf[k])) {
-            fprintf(stdout, "%c", asciibuf[k]);
-        } else {
-            fprintf(stdout, ".");
-        }
-    }
-
-    fprintf(stdout, "\n====== =======================   =======================  ================\n\n");
+    printf("%8s  =======================  =======================  %16s\n",
+           "========", "================");
+    return;
 }
